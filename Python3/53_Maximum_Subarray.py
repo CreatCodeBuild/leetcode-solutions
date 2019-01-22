183d55fc21fdf001e0950e9922927c448b5a7a57
CreatCodeBuild <CreatCodeBuild@github.com>
2019-01-21 20:49:06 -0800

    with subarray index

--- Python3/53_Maximum_Subarray.py
+++ Python3/53_Maximum_Subarray.py
@@ -1,23 +1,48 @@
 # https://leetcode.com/problems/maximum-subarray/
+# faster than 100%
 
 class Solution1:
 
-    def maxSubArray(self, a): 
-        
+    def maxSubArray(self, a):
+        """
+        just the sum
+        """
+
         max_so_far = a[0]
-        max_ending_here = a[0] if a[0] > 0 else 0
+        max_ending_here = a[0]
 
         for i in range(1, len(a)):
+            if max_ending_here < 0: 
+                max_ending_here = 0
             
             max_ending_here = max_ending_here + a[i]
             if max_ending_here > max_so_far:
                 max_so_far = max_ending_here
-    
+
+        return max_so_far
+
+    def max_subarray(self, a):
+        """
+        the sum and the subarray index
+        """
+        max_so_far = a[0]
+        max_ending_here = a[0]
+        s = 0
+        e = 0 
+
+        for i in range(1, len(a)):
+
             if max_ending_here < 0: 
                 max_ending_here = 0
+                s = i
+            
+            max_ending_here = max_ending_here + a[i]
+            if max_ending_here > max_so_far:
+                max_so_far = max_ending_here
+                e = i
 
-        return max_so_far 
+        return max_so_far, s, e
 
 
 data = [-1, 2,1]
-print(Solution1().maxSubArray(data))
+print(Solution1().max_subarray(data))
